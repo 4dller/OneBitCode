573b7c2b806b3684850ece1fe1bb1afb0dc7e343
4dller <105998603+4dller@users.noreply.github.com>
2024-02-12 22:31:20 -0300

    :books: First exercise on JS, calculator and User Register

---
+++ JS/exercise_01/calculadora.js
@@ -0,0 +1,21 @@
+const entrada1 = prompt (" Imforme o primeiro número: ")
+const entrada2 = prompt (" Informe o segundo número ")
+
+const x = parseFloat (entrada1)
+const y = parseFloat (entrada2)
+
+const soma = x + y
+const subtração = x - y
+const multiplicação = x * y
+const divisão = x / y
+
+
+alert (
+    "Resultados: \n" +
+
+    "\n Soma: " + soma +
+    "\n Subtração: " + subtração +
+    "\n Multiplicação: " + multiplicação +
+    "\n Divisão: " + divisão
+)
+
