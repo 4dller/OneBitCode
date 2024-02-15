b3204057e66fce597008a9ab35147c02f9a74bd7
4dller <105998603+4dller@users.noreply.github.com>
2024-02-15 17:31:11 -0300

    :books: Adding comments to my exs

--- JS/exercise_06/index.js
+++ JS/exercise_06/index.js
@@ -1,8 +1,17 @@
+
+// Variavel para pergunta o nome do turista
 const turista = prompt ("Eae turista, qual é o seu nome?")
 
+// Variavel vazia, para ser concatenada com as entradas de informações do usuario
 let cidades = ""
+
+// Variavel para a contagem de numero de cidades visitadas pelo turista
 let contagem = 0
-let continuar = prompt ("Qual cidade/país voce visitou? (Sim/Não)")
+
+// Variavel para salvar uma string, que será a resposta do usuario usada para o funcionamento do laço de repeticção while
+let continuar = prompt ("Você visitou alguma cidade? (Sim/Não)")
+
+
 
 while (continuar === "Sim")
 {
