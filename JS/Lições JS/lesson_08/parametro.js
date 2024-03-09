68b45a4e1c097034369ada36fe4b8332b88af8f5
4dller <105998603+4dller@users.noreply.github.com>
2024-03-09 19:19:52 -0300

    :books:Function parameters lesson

---
+++ JS/Lições JS/lesson_08/parametro.js
@@ -0,0 +1,14 @@
+function dobro(x){
+    alert("O dobro de " + x + " é " + (x* 2))
+}
+
+dobro(5)
+
+
+function dizerOla( nome = "mundo!") {
+
+    alert("Olá " + nome + ", Seja Bem vindo!")
+}
+
+dizerOla ("Adller")
+dizerOla ()
