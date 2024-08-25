3a9074a386f76b7f68108b971c27e25958330f5a
4dller <105998603+4dller@users.noreply.github.com>
2024-08-25 16:03:52 -0300

    :books: Atualização do ex 15 e aula de eventos

--- JS/Exercicios/exercicio_15 (Cadastro de vagas)/vagas.js
+++ JS/Exercicios/exercicio_15 (Cadastro de vagas)/vagas.js
@@ -27,8 +27,15 @@ if (confirmacao) {
     alert("Vaga criada!")
 }
 
+}
+
 function exibirVaga() {
     const indice = prompt("Informe o indice da vaga que deseja exibir:")
+    if (vagas.indice >= vagas.length || indice < 0) {
+        alert("Indice invalido.")
+        return
+    } 
+    
     const vaga = vagas[indice]
 
     const candidatosEmTexto = vaga.condidatos.reduce(function (textoFinal, candidato){
@@ -45,4 +52,80 @@ function exibirVaga() {
     )
 }
 
+function inscreverCandidato() {
+    const candidato = prompt("Informe o nome do(a) candidato(a):")
+    const indice = prompt("Informe o indice da vaga para qual o(a) candidato(a) deseja se inscrever:")
+    const vaga = vagas[indice]
+
+    const confirmacao = confirm (
+        "Deseja inscrever o candidato(a)" + candidato + " na vaga " + indice + "?\n" +
+        "Nome: " + vaga.nome + "\nDescrição: " + vaga.descricao + "\nData limite: " + vaga.dataLimite 
+    )
+
+    if (confirmacao) {
+        vaga.candidatos.push(candidato)
+        alert("Inscrição realizada!")
+    }    
 }
+
+function excluirVaga() {
+    const indice = prompt("Informe o índice da vaga que deseja excluir:")
+    const vaga = vagas[indice]
+
+    const confirmacao = confirm ("Tem certeza que deseja excluir a vaga " + indice + "?\n" + 
+        "Nome: " + vaga.nome + "\nDescrição: " + vaga.descricao + "\nData limite: " +vaga.dataLimite
+    )
+
+    if (confirmacao) {
+        vagas.splice(indice, 1)
+        alert ("Vaga excluída.")
+    }
+}
+
+function exibirMenu() {
+    const opcao = prompt(
+        "Cadastro de Vagas de Emprego" +
+        "\n\nEscolha uma das opções" +
+        "\n1. Listar vagas disponíveis" +
+        "\n2. Criar uma nova vaga" +
+        "\n3. Visualizar uma vaga" +
+        "\n4. Inscrever um(a) candidato(a)" +
+        "\n5. Excluir uma vaga" +
+        "\n6. Sair"
+      )
+
+      return opcao
+}
+
+function executar() {
+    let opcao = ""
+  
+    do {
+      opcao = exibirMenu()
+  
+      switch (opcao) {
+        case "1":
+          listarVagas()
+          break
+        case "2":
+          novaVaga()
+          break
+        case "3":
+          exibirVaga()
+          break
+        case "4":
+          inscreverCandidato()
+          break
+        case "5":
+          excluirVaga()
+        case "6":
+          alert("Saindo...")
+          break
+        default:
+          alert("Opção inválida.")
+      }
+  
+    } while (opcao !== "6");
+  }
+  
+  executar()
